82d465baf2be3922aeaec7d3c79c045dfca2cb6f
stream1080 <fmxhax638@foxmail.com>
2023-02-15 22:05:56 +0800

    feat: 增加 main.go 启动入口

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/stream1080/go-redis/config"
+	"github.com/stream1080/go-redis/lib/logger"
+	"github.com/stream1080/go-redis/tcp"
+)
+
+const configFileName string = "redis.conf"
+
+// 默认配置
+var defaultProperties = &config.ServerProperties{
+	Bind: "0.0.0.0",
+	Port: 6379,
+}
+
+func fileExists(fileName string) bool {
+	info, err := os.Stat(fileName)
+	return err == nil && !info.IsDir()
+}
+
+func main() {
+
+	// 默认配置
+	logger.Setup(&logger.Settings{
+		Path:       "logs",
+		Name:       "godis",
+		Ext:        "log",
+		TimeFormat: "2006-01-02",
+	})
+
+	if fileExists(configFileName) {
+		config.SetupConfig(configFileName)
+	} else {
+		config.Properties = defaultProperties
+	}
+
+	// 启动服务
+	err := tcp.ListenAndServeWithSignal(&tcp.Config{
+		Address: fmt.Sprintf("%s:%d", config.Properties.Bind, config.Properties.Port),
+	}, tcp.MakeEchoHandler())
+
+	if err != nil {
+		logger.Error(err)
+	}
+}

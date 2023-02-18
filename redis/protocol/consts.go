46fe36df373a160ed1e38fbcd705cdbb773361a2
stream1080 <fmxhax638@foxmail.com>
2023-02-18 21:36:18 +0800

    feat: 增加 一些常量回复

---
+++ redis/protocol/consts.go
@@ -0,0 +1,61 @@
+package protocol
+
+type PongReply struct{}
+
+var pongBytes = []byte("+Pong\r\n")
+
+func (r *PongReply) ToBytes() []byte {
+	return pongBytes
+}
+
+// ok 回复
+type OkReply struct{}
+
+var okBytes = []byte("+OK\r\n")
+
+func (r *OkReply) ToBytes() []byte {
+	return okBytes
+}
+
+func MakeOkReply() *OkReply {
+	return &OkReply{}
+}
+
+// 空字符串
+type NullBulkReply struct{}
+
+var nullBulkBytes = []byte("$-1\r\n")
+
+func (r *NullBulkReply) ToBytes() []byte {
+	return nullBulkBytes
+}
+
+func MakeNullBulkReply() *NullBulkReply {
+	return &NullBulkReply{}
+}
+
+// 空数组
+type EmptyMultiBulkReply struct{}
+
+var emptyMultiBulkBytes = []byte("*0\r\n")
+
+func (r *EmptyMultiBulkReply) ToBytes() []byte {
+	return emptyMultiBulkBytes
+}
+
+func MakeEmptyMultiBulkReply() *EmptyMultiBulkReply {
+	return &EmptyMultiBulkReply{}
+}
+
+// 无回复
+type NoReply struct{}
+
+var noBytes = []byte("")
+
+func (r *NoReply) ToBytes() []byte {
+	return noBytes
+}
+
+func MakeNoReply() *NoReply {
+	return &NoReply{}
+}

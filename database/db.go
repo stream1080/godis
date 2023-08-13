dd8d738a0858a8964b2e7448afbc47432e423581
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-08-13 16:56:27 +0800

    feat: complete db put and remove

--- database/db.go
+++ database/db.go
@@ -1,10 +1,12 @@
 package database
 
 import (
+	"strings"
+
 	"github.com/stream1080/godis/datastruct/dict"
+	"github.com/stream1080/godis/interface/database"
 	"github.com/stream1080/godis/interface/resp"
 	"github.com/stream1080/godis/resp/reply"
-	"strings"
 )
 
 type DB struct {
@@ -43,4 +45,45 @@ func validateArity(arity int, cmdArgs [][]byte) bool {
 	}
 
 	return argNum >= -arity
+}
+
+func (db *DB) GetEntity(key string) (*database.DataEntity, bool) {
+	raw, ok := db.data.Get(key)
+	if !ok {
+		return nil, false
+	}
+	return raw.(*database.DataEntity), true
+}
+
+func (db *DB) PutEntity(key string, entity *database.DataEntity) int {
+	return db.data.Put(key, entity)
+}
+
+func (db *DB) PutIfExists(key string, entity *database.DataEntity) int {
+	return db.data.PutIfExists(key, entity)
+}
+
+func (db *DB) PutIfAbsen(key string, entity *database.DataEntity) int {
+	return db.data.PutIfAbsent(key, entity)
+}
+
+func (db *DB) Remove(key string) {
+	db.data.Remove(key)
+}
+
+func (db *DB) Removes(keys ...string) int {
+	deleted := 0
+	for _, key := range keys {
+		_, exists := db.data.Get(key)
+		if exists {
+			db.Remove(key)
+			deleted++
+		}
+	}
+
+	return deleted
+}
+
+func (db *DB) Flush() {
+	db.data.Clear()
 }

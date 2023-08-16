b330e63a146ca6d0f760b096543219cb6ad609e7
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-08-16 23:01:04 +0800

    feat: add string set exec

--- database/string.go
+++ database/string.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/stream1080/godis/interface/database"
 	"github.com/stream1080/godis/interface/resp"
 	"github.com/stream1080/godis/resp/reply"
 )
@@ -13,4 +14,15 @@ func ExecGet(db *DB, args [][]byte) resp.Reply {
 	}
 
 	return reply.MakeBulkReply(entity.Data.([]byte))
+}
+
+func ExecSet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity := &database.DataEntity{
+		Data: args[1],
+	}
+
+	db.PutEntity(key, entity)
+
+	return reply.MakeOkReply()
 }

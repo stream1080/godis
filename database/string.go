d754842fbe6957e551055691124a0ceef90532eb
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-08-17 22:32:20 +0800

    feat: add getset and strlen exec

--- database/string.go
+++ database/string.go
@@ -35,3 +35,24 @@ func ExecSetNX(db *DB, args [][]byte) resp.Reply {
 
 	return reply.MakeIntReply(int64(db.PutIfAbsen(key, entity)))
 }
+
+func ExecGetSet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity, exists := db.GetEntity(key)
+	db.PutEntity(key, &database.DataEntity{Data: args[1]})
+	if !exists {
+		return reply.MakeNullBulkReply()
+	}
+
+	return reply.MakeBulkReply(entity.Data.([]byte))
+}
+
+func ExecStrLen(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity, exists := db.GetEntity(key)
+	if !exists {
+		return reply.MakeNullBulkReply()
+	}
+
+	return reply.MakeIntReply(int64(len(entity.Data.([]byte))))
+}

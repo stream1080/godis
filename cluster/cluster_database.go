0c2e2155e4d99945c9fb19850dfc41cc6a2e5bb8
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-10-01 12:03:21 +0800

    feat: impl MakeClusterDatabases()

---
+++ cluster/cluster_database.go
@@ -0,0 +1,55 @@
+package cluster
+
+import (
+	"context"
+	pool "github.com/jolestar/go-commons-pool/v2"
+
+	"github.com/stream1080/godis/config"
+	database2 "github.com/stream1080/godis/database"
+	"github.com/stream1080/godis/interface/database"
+	"github.com/stream1080/godis/interface/resp"
+	"github.com/stream1080/godis/lib/consistenthash"
+)
+
+type ClusterDatabases struct {
+	self     string
+	nodes    []string
+	peerPick *consistenthash.NodeMap
+	peerConn map[string]*pool.ObjectPool
+	db       database.Database
+}
+
+func MakeClusterDatabases() *ClusterDatabases {
+	cluster := &ClusterDatabases{
+		self:     config.Properties.Self,
+		db:       database2.NewStandaloneDatabase(),
+		peerPick: consistenthash.NewNodeMap(nil),
+		peerConn: make(map[string]*pool.ObjectPool),
+	}
+	nodes := make([]string, 0, len(config.Properties.Peers)+1)
+	for _, peer := range config.Properties.Peers {
+		nodes = append(nodes, peer)
+	}
+	nodes = append(nodes, config.Properties.Self)
+	cluster.peerPick.AddNode(nodes...)
+	ctx := context.Background()
+	for _, peer := range config.Properties.Peers {
+		cluster.peerConn[peer] = pool.NewObjectPoolWithDefaultConfig(ctx, &ConnFactory{
+			Peer: peer,
+		})
+	}
+	cluster.nodes = nodes
+	return cluster
+}
+
+func (c *ClusterDatabases) Exec(client resp.Connection, args [][]byte) resp.Reply {
+	panic("implement me")
+}
+
+func (c *ClusterDatabases) Close() {
+	panic("implement me")
+}
+
+func (c *ClusterDatabases) AfterClientClose(conn resp.Connection) {
+	panic("implement me")
+}

f198c43fb1776a26d2eec4d59865c0f1d0670b87
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-10-02 12:40:51 +0800

    feat: add MakeRouter func to router

--- cluster/router.go
+++ cluster/router.go
@@ -6,3 +6,15 @@ func defaultFunc(cluster *ClusterDatabases, c resp.Connection, args [][]byte) re
 	peer := cluster.peerPick.PickNode(string(args[1]))
 	return cluster.relay(peer, c, args)
 }
+
+func MakeRouter() map[string]CmdFunc {
+	routerMap := make(map[string]CmdFunc)
+	routerMap["exists"] = defaultFunc
+	routerMap["type"] = defaultFunc
+	routerMap["set"] = defaultFunc
+	routerMap["setnx"] = defaultFunc
+	routerMap["get"] = defaultFunc
+	routerMap["getset"] = defaultFunc
+
+	return routerMap
+}
